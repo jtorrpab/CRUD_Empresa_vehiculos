78dda286de222d23f70b97bd31579cb52fa7b546
jtorrpab <juanpablotorresp.17@gmail.com>
2022-10-11 12:59:41 -0500

    ajuste en App

--- src/main/java/com/empresa/App.java
+++ src/main/java/com/empresa/App.java
@@ -5,7 +5,6 @@ public class App
     public static void main( String[] args )
     {
         Empresa empresa1 = new Empresa("Ombe","123","cra 57","3122131");
-
-        empresa1. Solicitar_datos();
+        empresa1.Solicitar_datos();
     }
 }
